5ae82516d40543a7e0d6e414329234c4a7cab236
X-F-Cui <86182191+X-F-Cui@users.noreply.github.com>
2021-08-01 18:47:25 -0700

    Update MH.py
    Changed p_MHvar initialization to reflect updates

--- MH.py
+++ MH.py
@@ -35,8 +35,12 @@ def MH(data, models, delta, epsilon, gammas, iterations, isDelta):
 	#Initialize a random value of epsilon
 	MHvar = random.random()
 	
-	#Use pdf.m to calculate logs of height of epsilon on curve proportional to pdf over epsilon
-	p_MHvar = pdf(data, models, delta, epsilon, gammas)
+	#Determine whether the variable being sampled with MH sampling is delta or epsilon
+	if isDelta:
+		#Use pdf.m to calculate logs of height of epsilon on curve proportional to pdf over epsilon
+		p_MHvar = pdf(data, models, MHvar, epsilon, gammas)
+	else:
+		p_MHvar = pdf(data, models, delta, MHvar, gammas)
 	
 	timelog = [MHvar]
 	
