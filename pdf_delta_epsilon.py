3c939656387b73456be7ff3bf910d2d42289987b
X-F-Cui <86182191+X-F-Cui@users.noreply.github.com>
2021-08-03 00:18:29 -0700

    Update pdf_delta_epsilon.py
    Updates 08/03/2021: create function likelihoods (identical to original pdf_delta and pdf_epsilon function from line 43 to line 198), which calculates the M1likelihood, M2likelihood, M3likelihood of a given verb. Here, M1dict, M2dict, M3dict are passed in as arguments because we need to save these values globally, just like gammas.

--- pdf_delta_epsilon.py
+++ pdf_delta_epsilon.py
@@ -20,203 +20,216 @@
 #Updates 07/29/2021: merged pdf_delta and pdf_epsilon (which are identical except for the order of delta and epsilon 
 #    in the input argument and in the beginning when checking if delta/epsilon is between (0, 1)) into one file and updated variable names in MH_delta and MH_epsilon
 
+#Updates 08/03/2021: create function likelihoods (identical to original pdf_delta and pdf_epsilon function from line 43 to line 198), which calculates the M1likehood,
+#    M2likelihood, M3likelihood of a given verb. Here, M1dict, M2dict, M3dict are passed in as arguments because we need to save these values globally, just like gammas. 
+
+
 import math
 import numpy as np
 import itertools
 from operator import add
 
+
+def likelihoods(verb, delta, epsilon, gammas, M1dict, M2dict, M3dict):
+
+    k = verb[0]
+    n = verb[1]
+
+    M1component = []
+    M2component = []
+    M3component = []
+
+    ## likelihood p(k|T, epsilon, delta)
+    ## create tuples containing all combinations of n1 in range (0, n+1) and k1 in range (0, k+1)
+    ## equivalent to "for n1 in range (0, n+1) for k1 in range (0, k+1)"
+    n1 = range(n+1)
+    k1 = range(k+1)
+    combinations = list(itertools.product(n1, k1))
+
+    def calculate_k2(n1, k1):
+        if (k-k1)<=(n-n1):
+            if ((k-k1), (n-n1)) in gammas:
+                k2term = gammas[((k-k1), (n-n1))]+(k-k1)*np.log(delta)+((n-n1)-(k-k1))*np.log(1-delta)
+            else:
+                gammas[((k-k1), (n-n1))] = math.lgamma(n-n1+1)-(math.lgamma(k-k1+1)+math.lgamma((n-n1)-(k-k1)+1))
+                k2term = gammas[((k-k1), (n-n1))]+(k-k1)*np.log(delta)+((n-n1)-(k-k1))*np.log(1-delta)
+
+        else:
+           k2term = float('-inf')
+    
+        return k2term
+
+    def calculate_M1k1(n1, k1):
+        if (n1, k1) in M1dict:
+            M1k1term = M1dict[(n1, k1)]
+    
+        else:
+            if k1 <= n1:
+                if k1 == n1:
+                    M1k1term = 0
+                else:
+                    M1k1term = float('-inf')
+            else:
+                M1k1term = float('-inf')
+            M1dict[(n1, k1)] = M1k1term
+    
+        return M1k1term
+
+    def calculate_M2k1(n1, k1):
+        if (n1, k1) in M2dict:
+            M2k1term = M2dict[(n1, k1)]
+
+        else:
+            if k1 <= n1:
+                if k1 == 0:
+                    M2k1term = 0
+                else:
+                    M2k1term = float('-inf')
+
+            else:
+                M2k1term = float('-inf')
+        
+            M2dict[(n1, k1)] = M2k1term
+    
+        return M2k1term
+
+    def calculate_M3k1(n1, k1):
+        if (n1, k1) in M3dict:
+            M3k1term = M3dict[(n1, k1)]
+
+        else:
+            if k1 <= n1:
+                M3k1term = np.log(1.0)-np.log(n1+1)
+            else:
+                M3k1term = float('-inf')
+        
+            M3dict[(n1, k1)] = M3k1term
+
+        return M3k1term
+    
+    ## group by n1s
+    for key, group in itertools.groupby(combinations, lambda x: x[0]):
+        ngroup = list(group)
+        k2term = list(itertools.starmap(calculate_k2, ngroup))
+        M1k1term = list(itertools.starmap(calculate_M1k1, ngroup))
+        M2k1term = list(itertools.starmap(calculate_M2k1, ngroup))
+        M3k1term = list(itertools.starmap(calculate_M3k1, ngroup))
+
+        M1term = list(map(add, M1k1term, k2term))
+        M2term = list(map(add, M2k1term, k2term))
+        M3term = list(map(add, M3k1term, k2term))
+
+        M1term.sort(reverse=True)
+        M2term.sort(reverse=True)
+        M3term.sort(reverse=True)
+
+        if M1term[0] == float('-inf'):
+            M1termsub = M1term
+
+        else:
+            M1termsub = [(i-M1term[0]) for i in M1term]
+
+        if M2term[0] == float('-inf'):
+            M2termsub = M2term
+
+        else:
+            M2termsub = [(i-M2term[0]) for i in M2term]
+
+        if M3term[0] == float('-inf'):
+            M3termsub = M3term
+
+        else:
+            M3termsub = [(i-M3term[0]) for i in M3term]
+
+        M1termexp = [math.exp(i) for i in M1termsub]
+        M2termexp = [math.exp(i) for i in M2termsub]
+        M3termexp = [math.exp(i) for i in M3termsub]
+
+        M1logsum = M1term[0] + np.log1p(sum(M1termexp[1:]))
+        M2logsum = M2term[0] + np.log1p(sum(M2termexp[1:]))
+        M3logsum = M3term[0] + np.log1p(sum(M3termexp[1:]))
+
+        if (key, n) in gammas:
+            noise = gammas[(key, n)]+key*math.log(1-epsilon)+(n-key)*math.log(epsilon)
+
+        else:
+            gammas[(key, n)] = math.lgamma(n+1)-(math.lgamma(key+1)+math.lgamma(n-key+1))
+            noise = gammas[(key, n)]+key*math.log(1-epsilon)+(n-key)*math.log(epsilon)
+
+        M1component.append(M1logsum + noise)
+        M2component.append(M2logsum + noise)
+        M3component.append(M3logsum + noise)
+
+    M1component.sort(reverse=True)
+    M2component.sort(reverse=True)
+    M3component.sort(reverse=True)
+
+    if M1component[0] == float('-inf'):
+        M1componentsub = M1component
+
+    else:
+        M1componentsub = [(i-M1component[0]) for i in M1component]
+
+    if M2component[0] == float('-inf'):
+        M2componentsub = M2component
+
+    else:
+        M2componentsub = [(i-M2component[0]) for i in M2component]
+
+    if M3component[0] == float('-inf'):
+        M3componentsub = M3component
+
+    else:
+        M3componentsub = [(i-M3component[0]) for i in M3component]
+
+    M1componentexp = [math.exp(i) for i in M1componentsub]
+    M2componentexp = [math.exp(i) for i in M2componentsub]
+    M3componentexp = [math.exp(i) for i in M3componentsub]
+
+    M1likelihood = M1component[0] + np.log1p(sum(M1componentexp[1:]))
+    M2likelihood = M2component[0] + np.log1p(sum(M2componentexp[1:]))
+    M3likelihood = M3component[0] + np.log1p(sum(M3componentexp[1:]))
+
+    return [M1likelihood, M2likelihood, M3likelihood]
+
+
 def pdf(data, models, delta, epsilon, gammas):
-	
-	# check if delta and epsilon are within valid probability range
-	if delta < 0 or epsilon < 0:
-		p = float('-inf')
-	elif delta > 1 or epsilon > 1:
-		p = float('-inf')
-	else:
-		verbposteriors = []
-		
-		M1dict = {}
-		M2dict = {}
-		M3dict = {}
-		
+
+    if delta < 0 or epsilon < 0:
+        p = float('-inf')
+    elif delta > 1 or epsilon > 1:
+        p = float('-inf')
+    else:
+        verbposteriors = []
+
+        M1dict = {}
+        M2dict = {}
+        M3dict = {}
+
         ## loop through every verb in dataset and calculate p(k|T,epsilon,delta)
         ## following likelihood function in Equation (8) in Perkins, Feldman & Lidz
-		for verb in range(0, len(models)):
-		
-			verbcount = data[verb]
-			k = verbcount[0]
-			n = verbcount[1]
-			
-			M1component = []
-			M2component = []
-			M3component = []
-			
-			## create tuples containing all combinations of n1 in range (0, n+1) and k1 in range (0, k+1)
-			## equivalent to "for n1 in range (0, n+1) for k1 in range (0, k+1)"
-			n1 = range(n+1)
-			k1 = range(k+1)
-			combinations = list(itertools.product(n1, k1))
-			
-			def calculate_k2(n1, k1):
-				if (k-k1)<=(n-n1):
-					if ((k-k1), (n-n1)) in gammas:
-						k2term = gammas[((k-k1), (n-n1))]+(k-k1)*np.log(delta)+((n-n1)-(k-k1))*np.log(1-delta)
-					else:
-						gammas[((k-k1), (n-n1))] = math.lgamma(n-n1+1)-(math.lgamma(k-k1+1)+math.lgamma((n-n1)-(k-k1)+1))
-						k2term = gammas[((k-k1), (n-n1))]+(k-k1)*np.log(delta)+((n-n1)-(k-k1))*np.log(1-delta)
-				
-				else:
-					k2term = float('-inf')
-				
-				return k2term
-			
-			def calculate_M1k1(n1, k1):
-				if (n1, k1) in M1dict:
-					M1k1term = M1dict[(n1, k1)]
-					
-				else:
-					if k1 <= n1:	
-						if k1 == n1:
-							M1k1term = 0
-						else:
-							M1k1term = float('-inf')
-					else:
-						M1k1term = float('-inf')
-					M1dict[(n1, k1)] = M1k1term
-					
-				return M1k1term
-			
-			def calculate_M2k1(n1, k1):
-				if (n1, k1) in M2dict:
-					M2k1term = M2dict[(n1, k1)]
+        for verb in range(0, len(models)):
 
-				else:				
-					if k1 <= n1:
-						if k1 == 0:
-							M2k1term = 0
-						else:
-							M2k1term = float('-inf')
-				
-					else:
-						M2k1term = float('-inf')
-				
-					M2dict[(n1, k1)] = M2k1term
-					
-				return M2k1term
-				
-			def calculate_M3k1(n1, k1):
-				if (n1, k1) in M3dict:
-					M3k1term = M3dict[(n1, k1)]
-					
-				else:
-					if k1 <= n1:
-						M3k1term = np.log(1.0)-np.log(n1+1)
-					else:
-						M3k1term = float('-inf')
-						
-					M3dict[(n1, k1)] = M3k1term
-					
-				return M3k1term
-			
-			## group k1s by n1 values in order to efficiently compute inner sums in Equation (8)
-			for key, group in itertools.groupby(combinations, lambda x: x[0]):
-				ngroup = list(group)
-				k2term = list(itertools.starmap(calculate_k2, ngroup))
-				M1k1term = list(itertools.starmap(calculate_M1k1, ngroup))
-				M2k1term = list(itertools.starmap(calculate_M2k1, ngroup))
-				M3k1term = list(itertools.starmap(calculate_M3k1, ngroup))
-				
-				M1term = list(map(add, M1k1term, k2term))
-				M2term = list(map(add, M2k1term, k2term))
-				M3term = list(map(add, M3k1term, k2term))
-				
-				M1term.sort(reverse=True)
-				M2term.sort(reverse=True)
-				M3term.sort(reverse=True)
-			
-				if M1term[0] == float('-inf'):
-					M1termsub = M1term
-				
-				else:
-					M1termsub = [(i-M1term[0]) for i in M1term]
-				
-				if M2term[0] == float('-inf'):
-					M2termsub = M2term
+            verbcount = data[verb]
+		#given a verb, calculates the likelihoods over three categories
+            verbLikelihoods = likelihoods(verbcount, delta, epsilon, gammas, M1dict, M2dict, M3dict)
 
-				else:
-					M2termsub = [(i-M2term[0]) for i in M2term]
-				
-				if M3term[0] == float('-inf'):
-					M3termsub = M3term
-				
-				else:
-					M3termsub = [(i-M3term[0]) for i in M3term]
+            if models[verb] == 1:
+                verbposteriors.append(verbLikelihoods[0])
 
-				M1termexp = [math.exp(i) for i in M1termsub]
-				M2termexp = [math.exp(i) for i in M2termsub]
-				M3termexp = [math.exp(i) for i in M3termsub]
-			
-				M1logsum = M1term[0] + np.log1p(sum(M1termexp[1:]))
-				M2logsum = M2term[0] + np.log1p(sum(M2termexp[1:]))
-				M3logsum = M3term[0] + np.log1p(sum(M3termexp[1:]))
-				
-				if (key, n) in gammas:
-					noise = gammas[(key, n)]+key*math.log(1-epsilon)+(n-key)*math.log(epsilon)
-					
-				else:
-					gammas[(key, n)] = math.lgamma(n+1)-(math.lgamma(key+1)+math.lgamma(n-key+1))
-					noise = gammas[(key, n)]+key*math.log(1-epsilon)+(n-key)*math.log(epsilon)
-				
-				M1component.append(M1logsum + noise)
-				M2component.append(M2logsum + noise)
-				M3component.append(M3logsum + noise)
-			
-			M1component.sort(reverse=True)
-			M2component.sort(reverse=True)
-			M3component.sort(reverse=True)
-			
-			if M1component[0] == float('-inf'):
-				M1componentsub = M1component
-			
-			else:
-				M1componentsub = [(i-M1component[0]) for i in M1component]
-			
-			if M2component[0] == float('-inf'):
-				M2componentsub = M2component
-			
-			else:
-				M2componentsub = [(i-M2component[0]) for i in M2component]
-			
-			if M3component[0] == float('-inf'):
-				M3componentsub = M3component
+            elif models[verb] == 2:
+                verbposteriors.append(verbLikelihoods[1])
 
-			else:
-				M3componentsub = [(i-M3component[0]) for i in M3component]
+            elif models[verb] == 3:
+                verbposteriors.append(verbLikelihoods[2])
 
-			M1componentexp = [math.exp(i) for i in M1componentsub]
-			M2componentexp = [math.exp(i) for i in M2componentsub]
-			M3componentexp = [math.exp(i) for i in M3componentsub]
-			
-			M1likelihood = M1component[0] + np.log1p(sum(M1componentexp[1:]))
-			M2likelihood = M2component[0] + np.log1p(sum(M2componentexp[1:]))
-			M3likelihood = M3component[0] + np.log1p(sum(M3componentexp[1:]))
-			
-			if models[verb] == 1:
-				verbposteriors.append(M1likelihood)
-				
-			elif models[verb] == 2:
-				verbposteriors.append(M2likelihood)
-				
-			elif models[verb] == 3:
-				verbposteriors.append(M3likelihood)
-			
-			else:
-				print('Invalid model value')
+            else:
+                print('Invalid model value')
 
         ## function g(delta) in Equation (13) is equal to product across all verbs of likelihood term, times prior on delta
         ## prior is equal to 1 for all values of delta, because delta ~ Beta(1,1),
         ## so this reduces to product across all verbs of likelihood term
         ## and here, we're returning that value in log space
-		p = sum(verbposteriors)
-	
-	return p
+        p = sum(verbposteriors)
+
+    return p
