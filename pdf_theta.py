183dc07f3e49d2431b5723ab31b9b27aa9ea85bf
shalinee830 <maitra.shalinee00@gmail.com>
2021-08-05 12:17:59 -0400

    Update pdf_theta.py

--- pdf_theta.py
+++ pdf_theta.py
@@ -22,20 +22,22 @@ import numpy as np
 import itertools
 from operator import add
 
-def pdf_delta(data, models, epsilon, delta, gammas):
 
-	if delta <= 0:
+
+def pdf_theta(data, epsilon, delta,theta, gammas):
+
+	if theta <= 0:
 		p = float('-inf')
-	elif delta >= 1:
+	elif theta >= 1:
 		p = float('-inf')
 	else:
 		verbposteriors = []
 
-		M1dict = {}
-		M2dict = {}
+		#M1dict = {}
+		#M2dict = {}
 		M3dict = {}
 
-		## loop through every verb in dataset and calculate p(k|T,epsilon,delta)
+		## loop through every verb in dataset and calculate p(k|T,epsilon,delta) This is NOT what we want to do for theta
 		## following likelihood function in Equation (8) in Perkins, Feldman & Lidz
 		for verb in range(0, len(models)):
 
@@ -43,8 +45,8 @@ def pdf_delta(data, models, epsilon, delta, gammas):
 			k = verbcount[0]
 			n = verbcount[1]
 
-			M1component = []
-			M2component = []
+			# M1component = []
+			# M2component = []
 			M3component = []
 
 			## create (n1, k1) tuples containing all combinations of n1 in range (0, n+1) and k1 in range (0, k+1)
@@ -61,44 +63,45 @@ def pdf_delta(data, models, epsilon, delta, gammas):
 						gammas[((k-k1), (n-n1))] = math.lgamma(n-n1+1)-(math.lgamma(k-k1+1)+math.lgamma((n-n1)-(k-k1)+1))
 						k2term = gammas[((k-k1), (n-n1))]+(k-k1)*np.log(delta)+((n-n1)-(k-k1))*np.log(1-delta)
 
+
 				else:
 					k2term = float('-inf')
 
 				return k2term
 
-			def calculate_M1k1(n1, k1):
-				if (n1, k1) in M1dict:
-					M1k1term = M1dict[(n1, k1)]
+			# def calculate_M1k1(n1, k1):
+			# 	if (n1, k1) in M1dict:
+			# 		M1k1term = M1dict[(n1, k1)]
+			#
+			# 	else:
+			# 		if k1 <= n1:
+			# 			if k1 == n1:
+			# 				M1k1term = 0
+			# 			else:
+			# 				M1k1term = float('-inf')
+			# 		else:
+			# 			M1k1term = float('-inf')
+			# 		M1dict[(n1, k1)] = M1k1term
+			#
+			# 	return M1k1term
 
-				else:
-					if k1 <= n1:
-						if k1 == n1:
-							M1k1term = 0
-						else:
-							M1k1term = float('-inf')
-					else:
-						M1k1term = float('-inf')
-					M1dict[(n1, k1)] = M1k1term
-
-				return M1k1term
-
-			def calculate_M2k1(n1, k1):
-				if (n1, k1) in M2dict:
-					M2k1term = M2dict[(n1, k1)]
-
-				else:
-					if k1 <= n1:
-						if k1 == 0:
-							M2k1term = 0
-						else:
-							M2k1term = float('-inf')
-
-					else:
-						M2k1term = float('-inf')
-
-					M2dict[(n1, k1)] = M2k1term
-
-				return M2k1term
+			# def calculate_M2k1(n1, k1):
+			# 	if (n1, k1) in M2dict:
+			# 		M2k1term = M2dict[(n1, k1)]
+			#
+			# 	else:
+			# 		if k1 <= n1:
+			# 			if k1 == 0:
+			# 				M2k1term = 0
+			# 			else:
+			# 				M2k1term = float('-inf')
+			#
+			# 		else:
+			# 			M2k1term = float('-inf')
+			#
+			# 		M2dict[(n1, k1)] = M2k1term
+			#
+			# 	return M2k1term
 
 			def calculate_M3k1(n1, k1):
 				if (n1, k1) in M3dict:
@@ -106,7 +109,9 @@ def pdf_delta(data, models, epsilon, delta, gammas):
 
 				else:
 					if k1 <= n1:
-						M3k1term = np.log(1.0)-np.log(n1+1)
+						#this is the part that needs to be changed
+						#M3k1term = np.log(1.0)-np.log(n1+1)
+						k2term = gammas[((k1), (n1))]+(k1)*np.log(theta)+((n1)-(k1))*np.log(1-theta)
 					else:
 						M3k1term = float('-inf')
 
@@ -118,29 +123,29 @@ def pdf_delta(data, models, epsilon, delta, gammas):
 			for key, group in itertools.groupby(combinations, lambda x: x[0]):
 				ngroup = list(group)
 				k2term = list(itertools.starmap(calculate_k2, ngroup))
-				M1k1term = list(itertools.starmap(calculate_M1k1, ngroup))
-				M2k1term = list(itertools.starmap(calculate_M2k1, ngroup))
+				#M1k1term = list(itertools.starmap(calculate_M1k1, ngroup))
+				#M2k1term = list(itertools.starmap(calculate_M2k1, ngroup))
 				M3k1term = list(itertools.starmap(calculate_M3k1, ngroup))
 
-				M1term = list(map(add, M1k1term, k2term))
-				M2term = list(map(add, M2k1term, k2term))
+				#M1term = list(map(add, M1k1term, k2term))
+				#M2term = list(map(add, M2k1term, k2term))
 				M3term = list(map(add, M3k1term, k2term))
 
-				M1term.sort(reverse=True)
-				M2term.sort(reverse=True)
+				#M1term.sort(reverse=True)
+				#M2term.sort(reverse=True)
 				M3term.sort(reverse=True)
 
-				if M1term[0] == float('-inf'):
-					M1termsub = M1term
-
-				else:
-					M1termsub = [(i-M1term[0]) for i in M1term]
-
-				if M2term[0] == float('-inf'):
-					M2termsub = M2term
-
-				else:
-					M2termsub = [(i-M2term[0]) for i in M2term]
+				# if M1term[0] == float('-inf'):
+				# 	M1termsub = M1term
+				#
+				# else:
+				# 	M1termsub = [(i-M1term[0]) for i in M1term]
+				#
+				# if M2term[0] == float('-inf'):
+				# 	M2termsub = M2term
+				#
+				# else:
+				# 	M2termsub = [(i-M2term[0]) for i in M2term]
 
 				if M3term[0] == float('-inf'):
 					M3termsub = M3term
@@ -148,12 +153,12 @@ def pdf_delta(data, models, epsilon, delta, gammas):
 				else:
 					M3termsub = [(i-M3term[0]) for i in M3term]
 
-				M1termexp = [math.exp(i) for i in M1termsub]
-				M2termexp = [math.exp(i) for i in M2termsub]
+				# M1termexp = [math.exp(i) for i in M1termsub]
+				# M2termexp = [math.exp(i) for i in M2termsub]
 				M3termexp = [math.exp(i) for i in M3termsub]
 
-				M1logsum = M1term[0] + np.log1p(sum(M1termexp[1:]))
-				M2logsum = M2term[0] + np.log1p(sum(M2termexp[1:]))
+				# M1logsum = M1term[0] + np.log1p(sum(M1termexp[1:]))
+				# M2logsum = M2term[0] + np.log1p(sum(M2termexp[1:]))
 				M3logsum = M3term[0] + np.log1p(sum(M3termexp[1:]))
 
 				if (key, n) in gammas:
@@ -163,25 +168,25 @@ def pdf_delta(data, models, epsilon, delta, gammas):
 					gammas[(key, n)] = math.lgamma(n+1)-(math.lgamma(key+1)+math.lgamma(n-key+1))
 					noise = gammas[(key, n)]+key*math.log(1-epsilon)+(n-key)*math.log(epsilon)
 
-				M1component.append(M1logsum + noise)
-				M2component.append(M2logsum + noise)
+				# M1component.append(M1logsum + noise)
+				# M2component.append(M2logsum + noise)
 				M3component.append(M3logsum + noise)
 
-			M1component.sort(reverse=True)
-			M2component.sort(reverse=True)
+			# M1component.sort(reverse=True)
+			# M2component.sort(reverse=True)
 			M3component.sort(reverse=True)
 
-			if M1component[0] == float('-inf'):
-				M1componentsub = M1component
-
-			else:
-				M1componentsub = [(i-M1component[0]) for i in M1component]
-
-			if M2component[0] == float('-inf'):
-				M2componentsub = M2component
-
-			else:
-				M2componentsub = [(i-M2component[0]) for i in M2component]
+			# if M1component[0] == float('-inf'):
+			# 	M1componentsub = M1component
+			#
+			# else:
+			# 	M1componentsub = [(i-M1component[0]) for i in M1component]
+			#
+			# if M2component[0] == float('-inf'):
+			# 	M2componentsub = M2component
+			#
+			# else:
+			# 	M2componentsub = [(i-M2component[0]) for i in M2component]
 
 			if M3component[0] == float('-inf'):
 				M3componentsub = M3component
@@ -189,30 +194,29 @@ def pdf_delta(data, models, epsilon, delta, gammas):
 			else:
 				M3componentsub = [(i-M3component[0]) for i in M3component]
 
-			M1componentexp = [math.exp(i) for i in M1componentsub]
-			M2componentexp = [math.exp(i) for i in M2componentsub]
+			# M1componentexp = [math.exp(i) for i in M1componentsub]
+			# M2componentexp = [math.exp(i) for i in M2componentsub]
 			M3componentexp = [math.exp(i) for i in M3componentsub]
 
-			M1likelihood = M1component[0] + np.log1p(sum(M1componentexp[1:]))
-			M2likelihood = M2component[0] + np.log1p(sum(M2componentexp[1:]))
+			# M1likelihood = M1component[0] + np.log1p(sum(M1componentexp[1:]))
+			# M2likelihood = M2component[0] + np.log1p(sum(M2componentexp[1:]))
 			M3likelihood = M3component[0] + np.log1p(sum(M3componentexp[1:]))
 
-			if models[verb] == 1:
-				verbposteriors.append(M1likelihood)
-
-			elif models[verb] == 2:
-				verbposteriors.append(M2likelihood)
-
-			elif models[verb] == 3:
+			# if models[verb] == 1:
+			# 	verbposteriors.append(M1likelihood)
+			#
+			# elif models[verb] == 2:
+			# 	verbposteriors.append(M2likelihood)
+			#
 				verbposteriors.append(M3likelihood)
-
-			else:
-				print('Invalid model value')
+			#
+			# else:
+			# 	print('Invalid model value')
 
 		## function g(delta) in Equation (13) is equal to product across all verbs of likelihood term, times prior on delta
 		## prior is equal to 1 for all values of delta, because delta ~ Beta(1,1),
 		## so this reduces to product across all verbs of likelihood term
 		## and here, we're returning that value in log space
-		p = sum(verbposteriors)
+		verbposteriors
 
-	return p
+	return verbposteriors
