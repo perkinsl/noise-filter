acff6b98afff01019e4707532911f057caa35df5
X-F-Cui <86182191+X-F-Cui@users.noreply.github.com>
2021-09-08 00:13:01 -0700

    Update sample_models.py
    Updates 09/08/2021: added helper function proportionate_model_posterior to reduce repetitive code

--- sample_models.py
+++ sample_models.py
@@ -19,48 +19,42 @@
 
 #Updates 08/10/2021: got rid of the for loop within function sample_models
 
+#Updates 09/08/2021: added helper function proportionate_model_posterior to reduce repetitive code
+
 import math
 import random
 import numpy as np
 from pdf_delta_epsilon import likelihoods
 
+# calculating denominator for Equation (7)
+def proportionate_model_posterior(transitivity, verbLikelihoods):
+    # prior P(T) from Equation (7) is flat: 1/3 for each value of T
+    Mprior = 1.0/3.0
+    demM = verbLikelihoods[transitivity] + np.log(Mprior)
+    return demM
+
 def calculate_model(verbNumber, data, epsilon, delta, gammas, M1dict, M2dict, M3dict):
 
     verbcount = data[verbNumber]
 
-    ## prior P(T) from Equation (7) is flat: 1/3 for each value of T
-    M1prior = 1.0/3.0
-    M2prior = 1.0/3.0
-    M3prior = 1.0/3.0
-
     verbLikelihoods = likelihoods(verbcount, delta, epsilon, gammas, M1dict, M2dict, M3dict)
 
-    M1likelihood = verbLikelihoods[0]
-    M2likelihood = verbLikelihoods[1]
-    M3likelihood = verbLikelihoods[2]
-
-    ## these are the log numerators for all three verb categories in Equation (7)
-    demM1 = M1likelihood + np.log(M1prior)
-    demM2 = M2likelihood + np.log(M2prior)
-    demM3 = M3likelihood + np.log(M3prior)
-
-    ## calculating denominator for Equation (7)
-    dems = [demM1, demM2, demM3]
-    demsexp = [math.exp(i) for i in dems] ## list comprehension: [fun(i) for i in list]
+    #keeping this intermediate step here because it's needed when calculating models_posterior
+    dems = [proportionate_model_posterior(i, verbLikelihoods) for i in range(3)]
+    
+    demsexp = [math.exp(dem) for dem in dems] # list comprehension: [fun(i) for i in list]
     denominator = np.log(sum(demsexp))
 
-    ## final result of Equation (7), in log space
-    M1 = (M1likelihood + np.log(M1prior)) - denominator
-    M2 = (M2likelihood + np.log(M2prior)) - denominator
+    # final result of Equation (7), in log space
     #no need to calculate M3 since the prediction will automatically fall into M3 if it doesn't fit into M1 and M2
-    #M3 = (M3likelihood + np.log(M3prior)) - denominator
+    models_posterior = [(dems[i] - denominator) for i in range(2)]
 
     #flips a biased coin weighted by posteriors on models to sample a model (ie, category) for each verb
     x = random.random()
-    if x <= math.exp(M1):
+    if x <= math.exp(models_posterior[0]):
         return 1
 
-    elif x <= (math.exp(M1) + math.exp(M2)):
+    elif x <= (math.exp(models_posterior[0]) + math.exp(models_posterior[1])):
         return 2
 
     else:
