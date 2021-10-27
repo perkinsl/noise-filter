990635ff84793409e951390adb0cfd9a22cd3ca2
shalinee830 <78819836+shalinee830@users.noreply.github.com>
2021-10-27 10:28:41 -0700

    Update pdf_theta.py

--- pdf_theta.py
+++ pdf_theta.py
@@ -3,17 +3,16 @@
 #   1: verb is fully transitive (theta = 1)
 #   2: verb is fully intransitive (theta = 0)
 #   3: verb is mixed (theta sampled from Beta(1,1) uniform distribution)
+#Verbcount: the sublist with the counts of the data from the verb being sampled
 #This file assumes only one category, alternating verbs.
 #Data: a list of length n where each item is a 2-element list corresponding
 #   to counts of observations for each of n verbs. In each sublist, the first element
 #   contains counts of direct objects and the second contains total number of observations
 #Epsilon: a decimal from 0 to 1
+#Delta: a decimal from 0 to 1
+#Gammas: A dictionary to memoize all the combination terms in the likelihoods
 #Returns p, height of function proportional to pdf of posterior probability
 #   on delta, at specified value of delta
-
-
-## NOTE: a lot of this repeats code from sample_categories.py // THIS IS NO LONGER TRUE, RIGHT?
-## see comments in that code for details on how this corresponds to math in Perkins, Feldman, & Lidz // WE SHOULD PROBABLY COMMENT THE MATH HERE
 
 import math
 import numpy as np
@@ -23,7 +22,7 @@ from operator import add
 
 
 
-def pdf_theta_one_verb(verb, delta, epsilon, theta, gammas):
+def pdf_theta_one_verb(verbcount, delta, epsilon, theta, gammas):
 	if theta <= 0:
 		T3likelihood = float('-inf')
 	elif theta >= 1:
@@ -31,7 +30,6 @@ def pdf_theta_one_verb(verb, delta, epsilon, theta, gammas):
 
 	else:
 
-		verbcount = verb
 
 		k = verbcount[0]
 		n = verbcount[1]
@@ -121,21 +119,4 @@ def pdf_theta_one_verb(verb, delta, epsilon, theta, gammas):
 		T3likelihood = T3component[0] + np.log1p(sum(T3componentexp[1:]))
 	return T3likelihood
 
-#data = [[19, 20], [9, 10] ,[1, 20], [2, 40], [10, 20], [3, 10]]
-data = [[19, 20], [10, 10], [1, 20], [1, 40], [10, 20], [4, 10]]
-data3 = [[16, 20], [8, 10], [4, 20], [8, 40], [10, 20], [3, 10]]
-thetas = [0.95, 1.0, 0.05, 0.025, 0.5, 0.4]
-thetas3 = [0.8, 0.8, 0.2, 0.2, 0.5, 0.3]
-data4 = [[14, 20], [7, 10], [6, 20], [12, 40], [10, 20], [4, 10]]
-thetas4 = [0.7, 0.7,0.3, 0.3, 0.5, 0.4]
-data5 = [[11, 20],  [5, 10], [9, 20], [19, 40], [10, 20], [5, 10]]
-thetas5 = [0.55, 0.5, 0.45, 0.475, 0.5, 0.5]
-data6 = [[19, 20], [38, 40], [14, 15], [10, 10], [29, 30], [23, 25]]
-thetas6 = [0.95, 0.95, 0.933, 1.0, 0.966, 0.92]
-data7 =[[1, 20], [2, 40], [0, 15], [1, 10], [2, 30], [1, 25]]
-thetas7 = [0.05, 0.05, 0, 0.1, 0.0667, 0.04]
-data8 = [[7, 20], [6, 40], [7, 15], [4, 10], [7, 30], [19, 25]]
-thetas8 = [0.35, 0.15, 0.4667, 0.4, 0.233, 0.76]
 
-# pdf_theta_one_verb(data1, 0.01, 0.01, 0.95, {})
-#pdf_theta(data8, 0.01, 0.01, thetas8, {})
